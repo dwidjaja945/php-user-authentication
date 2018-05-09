dcf98825a9a3478b86396fa6ab0ff18e9d5ce968
Dylan Widjaja <dwidjaja945@gmail.com>
2018-05-09 13:07:03 -0700

    user authentication now fuctional

---
+++ user_data.php
@@ -0,0 +1,21 @@
+<?php
+
+$user_data = [
+    'dwidjaja' => [
+        'name' => 'Dylan',
+        'password' => 'password',
+        'user_level' => 2
+    ],
+    'jli' => [
+        'name' => 'Jasmine',
+        'password' => 'asdf',
+        'user_level' => 1
+    ],
+    'ewidjaja' => [
+        'name' => 'Evan',
+        'password' => 'widjaja',
+        'user_level' => 3
+    ]
+]
+
+?>
